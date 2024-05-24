c5033420d3b2135722d6b8d2d8043db9782d4aa6
Narayana Gaddipati <narayana.gaddipati@state.ca.gov>
2024-05-24 16:44:09 -0700

    Graph query works subject to privileges. Querying just for the graph admin name to prove.

--- AnA/AnA_MultiTenant/Program.cs
+++ AnA/AnA_MultiTenant/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Graph.Models.ExternalConnectors;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
+using Azure.Identity;
+using static System.Formats.Asn1.AsnWriter;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +27,19 @@ builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
 string? tenantId = builder.Configuration.GetValue<string>("AzureAd:TenantId");
 string? clientId = builder.Configuration.GetValue<string>("AzureAd:ClientId");
 string? clientSecret = builder.Configuration.GetValue<string>("AzureAd:ClientSecret");
+
+var clientSecretCredential = new ClientSecretCredential(
+tenantId, clientId, clientSecret);
+
+//var baseUrl = string.Join("/",
+//    builder.Configuration.GetSection("MicrosoftGraph")["BaseUrl"]);
+//var scopes = builder.Configuration.GetSection("DownstreamApi:Scopes")
+//    .Get<List<string>>();
+var scopes = new[] { ".default" };
+
+var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+builder.Services.AddSingleton(graphClient);
+
 
 //builder.Services.AddAuthorization(options =>
 //{
@@ -54,3 +69,28 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
+
+
+GraphServiceClient InitializeGraphClientAsync()
+{
+    var MyConfig = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+    // Values from app registration
+    var clientId = MyConfig.GetValue<string>("AzureAd:ClientId");
+    var clientSecret = MyConfig.GetValue<string>("AzureAd:ClientSecret");
+
+    //var scopes = new[] { "https://graph.microsoft.com/.default" };
+    var scopes = new[] { ".default" };
+
+    //// Multi-tenant apps can use "common",
+    //// single-tenant apps must use the tenant ID from the Azure portal
+    var tenantId = MyConfig.GetValue<string>("AzureAd:TenantId");
+
+    //// https://learn.microsoft.com/dotnet/api/azure.identity.clientsecretcredential
+    var clientSecretCredential = new ClientSecretCredential(
+        tenantId, clientId, clientSecret);
+
+    var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+
+    return graphClient;
+}
